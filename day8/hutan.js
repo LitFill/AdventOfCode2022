cefc29e00f0bdeeda77b5c7bc9d2a4b68119fca8
LitFill <98679748+LitFill@users.noreply.github.com>
2023-09-23 23:28:04 +0700

    finish day 8

--- day8/hutan.js
+++ day8/hutan.js
@@ -1,5 +1,6 @@
 import { parseLines as pl } from "../utils/index.js";
-import { lintas } from "../utils/index.js";
+import { matrixMax } from "../utils/index.js";
+// import { lintas } from "../utils/index.js";
 
 const lines = pl("./day8/day_8_input.txt", false);
 
@@ -17,15 +18,127 @@ for (let i = 0, len = lines.length; i < len; i++) {
     trees.push(baris);
 }
 
-// console.log(trees[4]);
+// console.log(trees[4][3]);
 // console.table(trees.splice(0, 5));
-console.log(lintas(trees, 1, 10));
+// console.log(lintas(trees, 1, 10));
 
-const jumlahPohonLuar = trees.length + trees[0].length - 1;
-// console.log(jumlahPohonLuar);
+const jumlahPohonLuar = 2 * (trees.length + trees[0].length - 2);
+console.log(jumlahPohonLuar);
 
-let pohonTerlihat = 0;
+console.log(
+    "part 1: jumlah pohon yang terlihat = %s.",
+    hitungPohonTerlihat(trees) + jumlahPohonLuar
+);
+// console.log(
+//     `part 1: jumlah pohon yang terlihat = ${
+//         hitungPohonTerlihat(trees) + jumlahPohonLuar
+//     }`
+// );
 
-for (let i = 1, n = trees.length; i < n - 1; i++) {
-    for (let j = 1, m = trees[i].length; j < m - 1; j++) {}
+console.log(
+    "part 2: skor pohon maksimal = %s.",
+    matrixMax(hitungSkorPohon(trees))
+);
+
+/**
+ * @description menghitung jumlah pohon yang terlihat dari luar hutan
+ * @author LitFill
+ * @date 23/09/2023
+ * @param {number[][]} grid
+ * @return {number} jumlah pohon yang terlihat
+ */
+function hitungPohonTerlihat(grid) {
+    const jumlahBaris = grid.length;
+    const jumlahKolom = grid[0].length;
+    let pohonTerlihat = 0;
+
+    for (let i = 1; i < jumlahBaris - 1; i++) {
+        for (let j = 1; j < jumlahKolom - 1; j++) {
+            const pohonSekarang = grid[i][j];
+            /** @type {number[]} */
+            const arrayArahTerlihat = new Array(4).fill(1);
+
+            for (let k = i - 1; k >= 0; k--) {
+                if (pohonSekarang <= grid[k][j]) {
+                    arrayArahTerlihat[0] = 0;
+                    break;
+                }
+            }
+            for (let k = i + 1; k < jumlahBaris; k++) {
+                if (pohonSekarang <= grid[k][j]) {
+                    arrayArahTerlihat[1] = 0;
+                    break;
+                }
+            }
+            for (let l = j - 1; l >= 0; l--) {
+                if (pohonSekarang <= grid[i][l]) {
+                    arrayArahTerlihat[2] = 0;
+                    break;
+                }
+            }
+            for (let l = j + 1; l < jumlahKolom; l++) {
+                if (pohonSekarang <= grid[i][l]) {
+                    arrayArahTerlihat[3] = 0;
+                    break;
+                }
+            }
+
+            if (arrayArahTerlihat.reduce((a, b) => a + b)) {
+                pohonTerlihat++;
+            }
+        }
+    }
+    return pohonTerlihat;
+}
+/**
+ * @description menghitung skor scenic pohon didalam grid
+ * @author LitFill
+ * @date 23/09/2023
+ * @param {number[][]} grid
+ * @return {number[][]}
+ */
+function hitungSkorPohon(grid) {
+    const jumlahBaris = grid.length;
+    const jumlahKolom = grid[0].length;
+    /** @type {number[][]} */
+    let skor = new Array(jumlahBaris);
+
+    for (let i = 0; i < jumlahBaris; i++) {
+        skor[i] = new Array(jumlahKolom);
+    }
+
+    for (let i = 0; i < jumlahBaris; i++) {
+        for (let j = 0; j < jumlahKolom; j++) {
+            const pohonSekarang = grid[i][j];
+            /** @type {number[]} */
+            const arraySkorPohon = new Array(4).fill(0);
+
+            for (let k = i - 1; k >= 0; k--) {
+                arraySkorPohon[0]++;
+                if (pohonSekarang <= grid[k][j]) {
+                    break;
+                }
+            }
+            for (let k = i + 1; k < jumlahBaris; k++) {
+                arraySkorPohon[1]++;
+                if (pohonSekarang <= grid[k][j]) {
+                    break;
+                }
+            }
+            for (let l = j - 1; l >= 0; l--) {
+                arraySkorPohon[2]++;
+                if (pohonSekarang <= grid[i][l]) {
+                    break;
+                }
+            }
+            for (let l = j + 1; l < jumlahKolom; l++) {
+                arraySkorPohon[3]++;
+                if (pohonSekarang <= grid[i][l]) {
+                    break;
+                }
+            }
+            skor[i][j] = arraySkorPohon.reduce((a, b) => a * b);
+        }
+    }
+    return skor;
 }
