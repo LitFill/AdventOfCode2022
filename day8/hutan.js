1e585c97857f2eb3c9ee615f2fa13694a3cf280c
LitFill <98679748+LitFill@users.noreply.github.com>
2023-09-23 23:43:04 +0700

    OPtimzed by gpt 3.5 but wrong

--- day8/hutan.js
+++ day8/hutan.js
@@ -4,26 +4,28 @@ import { matrixMax } from "../utils/index.js";
 
 const lines = pl("./day8/day_8_input.txt", false);
 
-/** @type {number[][]} */
-const trees = [];
-
-for (let i = 0, len = lines.length; i < len; i++) {
-    /** @type {number[]} */
-    const baris = [];
-
-    for (let j = 0; j < lines[i].length; j++) {
-        const pohon = Number(lines[i][j]);
-        baris.push(pohon);
-    }
-    trees.push(baris);
-}
+// /** @type {number[][]} */
+// const trees = [];
+//
+// for (let i = 0, len = lines.length; i < len; i++) {
+// /** @type {number[]} */
+// const baris = [];
+//
+// for (let j = 0; j < lines[i].length; j++) {
+//     const pohon = Number(lines[i][j]);
+//     baris.push(pohon);
+// }
+// trees.push(baris);
+// }
 
 // console.log(trees[4][3]);
 // console.table(trees.splice(0, 5));
 // console.log(lintas(trees, 1, 10));
 
+const trees = lines.map((line) => line.split("").map((char) => Number(char)));
+
 const jumlahPohonLuar = 2 * (trees.length + trees[0].length - 2);
-console.log(jumlahPohonLuar);
+// console.log(jumlahPohonLuar);
 
 console.log(
     "part 1: jumlah pohon yang terlihat = %s.",
@@ -38,6 +40,15 @@ console.log(
 console.log(
     "part 2: skor pohon maksimal = %s.",
     matrixMax(hitungSkorPohon(trees))
+);
+
+console.log(
+    "part 1 Opt: jumlah pohon yang terlihat = %s.",
+    hitungPohonTerlihatOpt(trees) + jumlahPohonLuar
+);
+console.log(
+    "part 2 Opt: skor pohon maksimal = %s.",
+    matrixMax(hitungSkorPohonOpt(trees))
 );
 
 /**
@@ -141,4 +152,112 @@ function hitungSkorPohon(grid) {
         }
     }
     return skor;
+}
+
+/**
+ * @description menghitung jumlah pohon yang terlihat dari luar hutan
+ * @param {number[][]} grid
+ * @return {number} jumlah pohon yang terlihat
+ */
+function hitungPohonTerlihatOpt(grid) {
+    const rows = grid.length;
+    const cols = grid[0].length;
+    let pohonTerlihat = 0;
+
+    grid.forEach((row, i) => {
+        row.forEach((pohonSekarang, j) => {
+            const visible = isPohonTerlihat(grid, i, j, rows, cols);
+            if (visible) {
+                pohonTerlihat++;
+            }
+        });
+    });
+
+    return pohonTerlihat;
+}
+
+/**
+ * @description Mengembalikan true jika pohon pada koordinat (i, j) terlihat dari luar hutan.
+ * @param {number[][]} grid
+ * @param {number} i
+ * @param {number} j
+ * @param {number} rows
+ * @param {number} cols
+ * @return {boolean}
+ */
+function isPohonTerlihat(grid, i, j, rows, cols) {
+    const pohonSekarang = grid[i][j];
+    const directions = [
+        [-1, 0],
+        [1, 0],
+        [0, -1],
+        [0, 1],
+    ];
+
+    for (const [dx, dy] of directions) {
+        let x = i + dx;
+        let y = j + dy;
+        while (x >= 0 && x < rows && y >= 0 && y < cols) {
+            if (grid[x][y] >= pohonSekarang) {
+                return false;
+            }
+            x += dx;
+            y += dy;
+        }
+    }
+
+    return true;
+}
+
+/**
+ * @description menghitung skor scenic pohon didalam grid
+ * @param {number[][]} grid
+ * @return {number[][]}
+ */
+function hitungSkorPohonOpt(grid) {
+    const rows = grid.length;
+    const cols = grid[0].length;
+    const skor = Array.from({ length: rows }, () => Array(cols).fill(0));
+
+    grid.forEach((row, i) => {
+        row.forEach((pohonSekarang, j) => {
+            const skorPohon = calculatePohonSkor(grid, i, j);
+            skor[i][j] = skorPohon;
+        });
+    });
+
+    return skor;
+}
+
+/**
+ * @description Menghitung skor pohon pada koordinat (i, j) berdasarkan ketinggian sekitarnya.
+ * @param {number[][]} grid
+ * @param {number} i
+ * @param {number} j
+ * @return {number}
+ */
+function calculatePohonSkor(grid, i, j) {
+    const pohonSekarang = grid[i][j];
+    const directions = [
+        [-1, 0],
+        [1, 0],
+        [0, -1],
+        [0, 1],
+    ];
+    let skorPohon = 1;
+
+    for (const [dx, dy] of directions) {
+        let x = i + dx;
+        let y = j + dy;
+        while (x >= 0 && x < grid.length && y >= 0 && y < grid[0].length) {
+            if (grid[x][y] >= pohonSekarang) {
+                break;
+            }
+            skorPohon++;
+            x += dx;
+            y += dy;
+        }
+    }
+
+    return skorPohon;
 }
